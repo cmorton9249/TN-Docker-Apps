cb4184b46e9d85716814c096e97b349947a63043
Chris Morton <christopher_r_morton@outlook.com>
2026-06-28 17:44:39 -0400

    Basic install of Home Assistant

--- Docker MacVLANs/macvlan.sh
+++ Docker MacVLANs/macvlan.sh
@@ -4,3 +4,9 @@ docker network create -d macvlan \
   --ip-range=192.168.1.192/25 \
   -o parent=br1 \
   primenet
+
+sudo docker network create -d macvlan \
+  --subnet=192.168.101.0/24 \
+  --gateway=192.168.101.1 \
+  -o parent=br101 \
+  ha-vlan
